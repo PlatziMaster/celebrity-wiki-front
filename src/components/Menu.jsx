c80643e1c10af605bf6fb31ee4615a4b75ef8bb5
ithzavg <ithgarvi@gmail.com>
2021-06-22 18:58:16 -0500

    Home styles added, favicon added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,15 +8,15 @@ const Menu=() =>{
     return(
         <section className="menu">
             <div className="menu__item">
-                <img src={NewsGreen} alt="Icon Celebrity news"/>
+                <img src={NewsGreen} alt="Icon Celebrity news" className="menu__icon"/>
                 <p>Celebrity</p>
             </div>
             <div className="menu__item">
-                <img src={Trends} alt="Icon Trends"></img>
+                <img src={Trends} alt="Icon Trends"  className="menu__icon"></img>
                 <p>Trends</p>
             </div>
             <div className="menu__item">
-                <img src={Categories} alt="Icon Categories"></img>
+                <img src={Categories} alt="Icon Categories" className="menu__icon"></img>
                 <p>Categories</p>
             </div>
 
--- src/views/Home/Homepage.jsx
+++ src/views/Home/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from "../../components/Card";
-import Header from "../../components/Header"
-import "../../styles/components/home/_home.scss"
+import Header from "../../components/Header";
+import "../../styles/components/home/_home.scss";
+import "../../styles/components/home/_home-desktop.scss";
 
 export const HomepageView = () => (
-    <main>
+    <main className="main">
       <Header></Header>
       <section className="cards">
+        <Card image="" title="Tesla failed to stop Musk tweets, says regulator" reads="5 mins read" author="Mary Walton"></Card>
         <Card image="" title="Tesla failed to stop Musk tweets, says regulator" reads="5 mins read" author="Mary Walton"></Card>
         <Card image="" title="Tesla failed to stop Musk tweets, says regulator" reads="5 mins read" author="Mary Walton"></Card>
       </section>
