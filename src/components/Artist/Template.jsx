2b4840ae720299320343a4f6152719e982d519bf
sebas rivera <bostjanrivera@gmail.com>
2021-07-01 18:07:09 -0500

    celebrity view updated

--- src/components/Artist/Template.jsx
+++ src/components/Artist/Template.jsx
@@ -8,12 +8,22 @@ export const Template = ({ artist, loading }) => (
     <div className="artist-info">
       <h2 className="artist--name">{artist.Artist_name}</h2>
       <ul className="artist-info__songs">
-        <li><b>Saddest song: </b>{artist.Analysis.Saddest.song_name}</li>
-        <li><b>Happiest song: </b>{artist.Analysis.Happiest.song_name}</li>
-        <li><b>total albums: </b>{artist.Total_albums}</li>
+        <li>
+          <b>Saddest song: </b>
+          {artist.Analysis.Saddest.song_name}
+        </li>
+        <li>
+          <b>Happiest song: </b>
+          {artist.Analysis.Happiest.song_name}
+        </li>
+        <li>
+          <b>total albums: </b>
+          {artist.Total_albums}
+        </li>
       </ul>
+      <h2 className="artist-info--albums">Albums</h2>
       <div className="album-container">
-        {artist.Albums_and_songs.map(album => (
+        {artist.Albums_and_songs.map((album) => (
           <div className="album-card" key={album.Id}>
             <div>
               <p>{album.Album_name}</p>
--- src/components/Celebrity/Template.jsx
+++ src/components/Celebrity/Template.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React from "react";
 
-export const Template = ({ celebrity, loading })  => (
+export const Template = ({ celebrity, loading }) => (
   <div className="celebrity">
-    <figure className="celebrity-image">
-      <img src={celebrity.Image} alt={celebrity.name} />
-    </figure>
-    <div className="celebrity-desc">
-      <h2>{celebrity.name}</h2>
-      <ul className="celebrity-desc__dates">
-        <li><b>Age: </b>{celebrity.age}</li>
-        <li><b>Height: </b>{celebrity.height}</li>
-        <li><b>Ocupation: </b>{celebrity.occupation}</li>
-        <li><b>Nationality: </b>{celebrity.nationality}</li>
+    <div className="celebrity-info">
+      <ul className="celebrity-info__list">
+        <li>
+          <b>Nationality:</b>
+          <p>{celebrity.nationality === undefined ? "no definido" : celebrity.nationality}</p>
+        </li>
+        <li>
+          <b>Gender:</b>
+          <p>{celebrity.gender === undefined ? "no definido" : celebrity.gender  }</p>
+        </li>
+        <li>
+          <b>Height:</b>
+          <p>{celebrity.height === undefined ? "no definido" : celebrity.height}</p>
+        </li>
+        <li>
+          <b>Birth date:</b>
+          <p>{celebrity.birthday === undefined ? "no definido" : celebrity.birthday  }</p>
+        </li>
+        <li>
+          <b>Age:</b>
+          <p>{celebrity.age === undefined ? "no definido" : celebrity.age}</p>
+        </li>
+        <li>
+          <b>Net worth:</b>
+          <p>{celebrity.net_worth === undefined ? "no definido" : celebrity.net_worth}</p>
+        </li>
+        <li>
+          <b>Ocuppation:</b>
+          <p>{celebrity.occupation === undefined ? "no definido" : celebrity.occupation  }</p>
+        </li>
       </ul>
-      <p className="celebrity-desc--bio">{celebrity.bio}</p>
     </div>
+    <div className="celebrity-header">
+      <div className="celebrity-header__name">
+        <h2>{celebrity.name}</h2>
+        <p>{celebrity.occupation}</p>
+      </div>
+      <figure>
+        <img src={celebrity.Image} alt="celebrity imae" />
+      </figure>
+    </div>
+    <p className="celebrity--bio">{celebrity.bio}</p>
   </div>
-)
+);
